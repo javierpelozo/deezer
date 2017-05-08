64de514724ed3cf9dcc8afc19ff08412c65f92cb
javierpelozo <javpelozo@gmail.com>
2017-05-08 15:50:04 -0400

    cambios

---
+++ src/pages/inicio/inicio.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the Inicio page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@Component({
+  selector: 'page-inicio',
+  templateUrl: 'inicio.html',
+})
+export class InicioPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('Inicio page está cargado');
+  }
+
+}
